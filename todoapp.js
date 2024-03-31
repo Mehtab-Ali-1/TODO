4549424799c49d981d5a00dbbef49cb528901995
Mehtab-Ali-1 <160261202+Mehtab-Ali-1@users.noreply.github.com>
2024-03-31 17:29:31 +0500

    Add files via upload

---
+++ todoapp.js
@@ -0,0 +1,20 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+while (condition) {
+    let add = await inquirer.prompt([{
+            name: `todos`,
+            type: `input`,
+            message: `What you want to add in your todays?`
+        }, {
+            name: `addmore`,
+            type: `confirm`,
+            message: `Do you want to add more?`,
+            default: `false`
+        }
+    ]);
+    todos.push(add.todos);
+    condition = add.addmore;
+    console.log(todos);
+}
